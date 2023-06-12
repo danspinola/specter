eceacbf13bcd096b8728c398f357bd6d9bfe76fe
Daniel Spinola <daniel.trindade1@ibm.com>
2023-06-12 02:10:52 -0300

    feat(utils): add `isMobileDevice`

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export function isMobileDevice(): boolean {
+	const userAgent = navigator.userAgent.toLowerCase();
+
+	return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+}
